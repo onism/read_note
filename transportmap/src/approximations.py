dc3065e6c70fad4c3562b96f61feb427c114f481
xiaowang <wangjunjie2013@gmail.com>
2017-10-11 08:48:50 +0800

    transport notes

---
+++ transportmap/src/approximations.py
@@ -0,0 +1,14 @@
+import logging
+import warnings
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+import SpectralToolbox.Spectral1D as S1D
+import TransportMaps as TM
+import TransportMaps.Functionals as FUNC
+import TransportMaps.Maps as MAPS
+warnings.simplefilter("ignore")
+TM.setLogLevel(logging.INFO)
+
+
+
---
+++ transportmap/src/low_dimensional_couplings.py
@@ -0,0 +1,10 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator, FormatStrFormatter
+import numpy as np
+import TransportMaps as TM
+import TransportMaps.Maps as MAPS
+import TransportMaps.Distributions as DIST
+import TransportMaps.Diagnostics as DIAG
+mpl.rcParams['font.size'] = 15
+
